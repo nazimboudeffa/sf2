7790946f8f480e355ec270a8aa89fdc34a1358bf
Nazim Boudeffa <boudeffa@gmail.com>
2019-05-16 01:26:42 +0200

    It's time to pass to a state machine

--- game.js
+++ game.js
@@ -3,6 +3,9 @@ var game = new Phaser.Game(512, 224, Phaser.AUTO, 'game', { init: init, preload:
 var player
 var ground
 var cursors
+var lpunchKey
+var jumpTime = 0
+var isCrouch = false, isIDLE = true;
 
 function init(){
 
@@ -43,8 +46,11 @@ function create(){
   player.animations.add('forward', ['walking1.png','walking2.png','walking3.png','walking4.png','walking5.png']);
   player.animations.add('backward', ['walking5.png','walking4.png','walking3.png','walking2.png','walking1.png']);
   player.animations.add('jump', ['jump1.png','jump2.png','jump3.png','jump4.png','jump5.png','jump6.png']);
+  player.animations.add('crouch', ['crouch1.png','crouch2.png'], false, false);
 
-  cursors = game.input.keyboard.createCursorKeys();
+  player.animations.add('lpunch', ['lpunch1.png','lpunch2.png','lpunch3.png'], false, false);
+
+  inputsDeclarations();
 
 }
 
@@ -61,17 +67,30 @@ function update(){
   {
     player.body.velocity.x = -80;
     player.animations.play('backward', 6, true);
-  } else if(cursors.up.isDown)
-  {
-    player.body.velocity.y = -80;
-    player.animations.play('jump', 6);
   }
-  else if(cursors.down.isDown)
+  else if(cursors.up.isDown && game.time.now > jumpTime)
   {
-
-  } else
+    player.body.velocity.y = -240;
+    player.animations.play('jump', 6);
+    jumpTime = game.time.now + 2000;
+  }
+  else if(cursors.down.isDown && game.time.now > jumpTime)
+  {
+    player.animations.play('crouch', 6);
+    player.body.velocity.x = 0;
+  }
+  else if (lightPunch.isDown)
+  {
+    player.animations.play('lpunch', 6);
+  }
+  else
   {
     player.body.velocity.x = 0;
-    player.animations.play('idle', 6, true);
+    player.animations.play('idle', 6);
   }
+}
+
+function inputsDeclarations() {
+    cursors = game.input.keyboard.createCursorKeys();
+    lightPunch = game.input.keyboard.addKey(Phaser.Keyboard.C);
 }
